4333cc2fed140d5af442df4e3b6bb917eed80e56
OliverJL <oliver-jessl@gmx.net>
2017-10-17 07:32:22 +0200

    implemented various sys_kernel_entropy_... calls

---
+++ exp/entropy_analysis.c
@@ -0,0 +1,58 @@
+/*
+ * entropy_analysis.c
+ *
+ *  Created on: Oct 14, 2017
+ *      Author: lvr
+ */
+
+
+
+#include<linux/kernel.h>
+#include<linux/init.h>
+#include<linux/sched.h>
+#include<linux/syscalls.h>
+#include <exp/entropy_analysis.h>
+
+
+asmlinkage long sys_kernel_entropy_get_size(void)
+{
+	spin_lock(&entropy_analysis_lock);
+	return kernel_entropy_record_size;
+	spin_unlock(&entropy_analysis_lock);
+}
+
+unsigned long recorded_kernel_entropy_size = 0;
+
+asmlinkage bool sys_kernel_entropy_get_recorded(process_kernel_entropy * target_buffer)
+{
+	if(target_buffer == NULL)
+		return 1;
+	spin_lock(&entropy_analysis_lock);
+	recorded_kernel_entropy_size = ((kernel_entropy_record_size + 1) * sizeof(process_kernel_entropy));
+	copy_to_user (target_buffer, &recorded_kernel_entropy, recorded_kernel_entropy_size);
+    spin_unlock(&entropy_analysis_lock);
+	return 0;
+}
+
+asmlinkage bool sys_kernel_entropy_start_recording(void)
+{
+	spin_lock(&entropy_analysis_lock);
+	is_kernel_entropy_recording = 1;
+	spin_unlock(&entropy_analysis_lock);
+	return 0;
+}
+
+asmlinkage bool sys_kernel_entropy_stop_recording(void)
+{
+	spin_lock(&entropy_analysis_lock);
+	is_kernel_entropy_recording = 0;
+	spin_unlock(&entropy_analysis_lock);
+	return 0;
+}
+
+asmlinkage bool sys_kernel_entropy_is_recording(void)
+{
+	spin_lock(&entropy_analysis_lock);
+	return is_kernel_entropy_recording;
+	spin_unlock(&entropy_analysis_lock);
+}
