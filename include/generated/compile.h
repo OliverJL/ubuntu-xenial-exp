9c8f7e3630b85b3225416a2b2ca132c5465b22ae
OliverJL <oliver-jessl@gmx.net>
2017-10-28 12:43:35 +0200

    print current_ke_record->stack_canary in hey instead dec format

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 62 */
+/* This file is auto generated, version 63 */
 /* SMP */
 #define UTS_MACHINE "x86_64"
-#define UTS_VERSION "#62 SMP Fri Oct 27 21:43:09 CEST 2017"
+#define UTS_VERSION "#63 SMP Sat Oct 28 12:42:38 CEST 2017"
 #define LINUX_COMPILE_BY "lvr"
 #define LINUX_COMPILE_HOST "wrksttt"
 #define LINUX_COMPILER "gcc version 5.4.0 20160609 (Ubuntu 5.4.0-6ubuntu1~16.04.4) "
