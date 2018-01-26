75d2ef9f3b95561cee33ce4aef8e4711502fd2cf
OliverJL <oliver-jessl@gmx.net>
2018-01-26 01:01:04 +0100

    sys_kernel_entropy_get_recorded copy_to_user

--- exp/entropy_analysis.c
+++ exp/entropy_analysis.c
@@ -166,7 +166,7 @@ asmlinkage long sys_kernel_entropy_get_recorded(kernel_entropy_event * tb_ke_eve
 	int kee_add_int_rnd_cntr = 0;
 	//int tb_kee_stc_set_cntr = 0;
 	kernel_entropy_event * ke_event;
-	//kernel_entropy_event * tb_kee;
+	kernel_entropy_event * tb_kee;
 	//kee_add_interrupt_rnd * trg_add_int_rnd_event;
 	kee_add_interrupt_rnd * kee_add_int_rnd;
 
@@ -175,9 +175,9 @@ asmlinkage long sys_kernel_entropy_get_recorded(kernel_entropy_event * tb_ke_eve
 	while(kee_rec_cntr < ke_rec_info.kee_rec_id )
 	{
 		ke_event = &recorded_kernel_entropy[kee_rec_cntr];
-		//tb_kee = &tb_ke_event[kee_rec_cntr];
+		tb_kee = &tb_ke_event[kee_rec_cntr];
 		//printk(KERN_EMERG ">>>>>> sys_kernel_entropy_get_recorded ke_event->id:%zu - ke_event->event_type:%zu", ke_event->id, ke_event->event_type);
-		//copy_to_user(&tb_ke_event[kee_rec_cntr], &ke_event, sizeof(kernel_entropy_event));
+		copy_to_user(&tb_ke_event[kee_rec_cntr], &ke_event, sizeof(kernel_entropy_event));
 
 		switch(ke_event->event_type)
 		{
