fac4fb74bd8bb725f75cc2e9941600a516d4cd2c
OliverJL <oliver-jessl@gmx.net>
2018-01-28 23:54:11 +0100

    mv copy_to_user

--- exp/entropy_analysis.c
+++ exp/entropy_analysis.c
@@ -189,7 +189,7 @@ asmlinkage long sys_kernel_entropy_get_recorded(kernel_entropy_event * tb_ke_eve
 		tb_kee = &tb_ke_event[kee_rec_cntr];
 		//printk(KERN_EMERG ">>>>>> sys_kernel_entropy_get_recorded ke_event->id:%zu - ke_event->event_type:%zu", ke_event->id, ke_event->event_type);
 		//copy_to_user(&tb_ke_event[kee_rec_cntr], &ke_event, sizeof(kernel_entropy_event));
-		copy_to_user(tb_kee, ke_event, sizeof(kernel_entropy_event));
+		//copy_to_user(tb_kee, ke_event, sizeof(kernel_entropy_event));
 
 		switch(ke_event->event_type)
 		{
@@ -211,9 +211,10 @@ asmlinkage long sys_kernel_entropy_get_recorded(kernel_entropy_event * tb_ke_eve
 			case KEETYPE__ADD_INT_RND__FAST_POOL_COMPLETE:
 			case KEETYPE__ADD_INT_RND__FAST_POOL_LT_64:
 			case KEETYPE__ADD_INT_RND__SPIN_TRYLOCK:
+				ke_event->detail_index = kee_add_int_rnd_cntr;
+				copy_to_user(tb_kee, ke_event, sizeof(kernel_entropy_event));
 				//printk(KERN_EMERG ">>>>>> KEETYPE__ADD_INT_RND__ kee_add_int_rnd_cntr:%d", kee_add_int_rnd_cntr);
 				//printk(KERN_EMERG ">>>>>> KEETYPE__ADD_INT_RND ke_event->detail_index = kee_add_int_rnd_cntr;");
-				ke_event->detail_index = kee_add_int_rnd_cntr;
 				//printk(KERN_EMERG ">>>>>> KEETYPE__ADD_INT_RND copy_to_user");
 				//copy_to_user(&tb_ke_event[kee_rec_cntr], ke_event, sizeof(kernel_entropy_event));
 				//copy_to_user(tb_kee, ke_event, sizeof(kernel_entropy_event));
@@ -268,8 +269,9 @@ asmlinkage long sys_kernel_entropy_get_recorded(kernel_entropy_event * tb_ke_eve
 				//printk(KERN_EMERG ">>>>>> KEETYPE__ADD_INT_RND__ $$$$$$$$");
 				//tb_kee->detail_index = kee_add_int_rnd_cntr;
 				//printk(KERN_EMERG ">>>>>> kee_add_int_rnd_cntr ++");
-				printk(KERN_EMERG ">>>>>> kee_add_int_rnd_cntr ++: %d", kee_add_int_rnd_cntr );
-				printk(KERN_EMERG ">>>>>> ke_event->detail_index ++: %d", ke_event->detail_index );
+				//printk(KERN_EMERG ">>>>>> kee_add_int_rnd_cntr ++: %d", kee_add_int_rnd_cntr );
+				//printk(KERN_EMERG ">>>>>> ke_event: 0x%08X", ke_event );
+				//printk(KERN_EMERG ">>>>>> ke_event->detail_index ++: %d", ke_event->detail_index );
 
 				kee_add_int_rnd_cntr ++;
 				break;
