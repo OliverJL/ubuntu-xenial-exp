59d68d09f8096aefb8bbf0eede99e9212882e577
OliverJL <oliver-jessl@gmx.net>
2018-02-03 19:30:34 +0100

    kernel_entropy_malloc_event debug cmt

--- exp/entropy_analysis.c
+++ exp/entropy_analysis.c
@@ -115,7 +115,7 @@ void kernel_entropy_rec_get_rnd_int(int pid, unsigned long jiffies, unsigned int
 	kernel_entropy_event * ke_event;
 	kee_get_rnd_int * get_rnd_int;
 
-	//ke_event = kernel_entropy_malloc_event(KEETYPE__GET_RANDOM_INT);
+	ke_event = kernel_entropy_malloc_event(KEETYPE__GET_RANDOM_INT);
 
 	if(ke_event != NULL)
 	{
