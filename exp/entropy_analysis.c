5256fd9a90238eb29749729da5fb573c08dfc5da
OliverJL <oliver-jessl@gmx.net>
2018-02-03 21:06:32 +0100

    comnt kernel_entropy_rec_get_rnd_int

--- exp/entropy_analysis.c
+++ exp/entropy_analysis.c
@@ -128,6 +128,7 @@ void kernel_entropy_rec_random_int_secret_set(u32 * random_int_secret)
 
 void kernel_entropy_rec_get_rnd_int(int pid, unsigned long jiffies, unsigned int rnd_raw, unsigned int rnd_final)
 {
+	/*
 	//
 	kernel_entropy_event * ke_event;
 	kee_get_rnd_int * get_rnd_int;
@@ -142,11 +143,12 @@ void kernel_entropy_rec_get_rnd_int(int pid, unsigned long jiffies, unsigned int
 		get_rnd_int->jiffies = jiffies;
 		get_rnd_int->rnd_raw = rnd_raw;
 		get_rnd_int->rnd_final = rnd_final;
-		*/
+		* /
 	}else
 	{
 		printk(KERN_EMERG ">>>>>> kernel_entropy_rec_get_rnd_int - ke_event == NULL!!!");
 	}
+	*/
 }
 
 
