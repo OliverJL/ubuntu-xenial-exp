45854be197a6c0310165f64fd05eb27a562c2f13
OliverJL <oliver-jessl@gmx.net>
2018-02-06 22:20:20 +0100

    fix? char * filename -> const char * filename

--- exp/entropy_analysis.c
+++ exp/entropy_analysis.c
@@ -132,7 +132,7 @@ void kernel_entropy_rec_random_int_secret_set(u32 * random_int_secret)
 extern int rec_aslr_set_filename_max;
 extern int rec_aslr_set_elf_interpreter_max;
 
-void kernel_entropy_rec_aslr_set(char * filename, char * elf_interpreter, int elf_prot, int elf_flags, unsigned long load_addr, unsigned long load_bias, unsigned long entry_point, unsigned long mmap_rnd, unsigned long vaddr, unsigned long start_code, unsigned long end_code, unsigned long start_data, unsigned long end_data, unsigned long error )
+void kernel_entropy_rec_aslr_set(const char * filename, char * elf_interpreter, int elf_prot, int elf_flags, unsigned long load_addr, unsigned long load_bias, unsigned long entry_point, unsigned long mmap_rnd, unsigned long vaddr, unsigned long start_code, unsigned long end_code, unsigned long start_data, unsigned long end_data, unsigned long error )
 {
 	kernel_entropy_event * ke_event;
 	kee_aslr_set * aslr_set;
