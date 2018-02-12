de9039e29cccf94f6ee167b8a1ea30536f3c8270
OliverJL <oliver-jessl@gmx.net>
2018-02-12 11:52:06 +0100

    fix? removed pragma pack(1) for struct kee_aslr_set

--- include/exp/entropy_analysis.h
+++ include/exp/entropy_analysis.h
@@ -77,11 +77,9 @@ typedef struct
 } kee_stack_canary_set;
 #pragma pack()
 
-#pragma pack(1)
+//#pragma pack(1)
 typedef struct
 {
-//   char filename[100];
-//   char elf_interpreter[100];
    int pid;
    int elf_prot;
    int elf_flags;
@@ -96,7 +94,7 @@ typedef struct
    unsigned long end_data;
    unsigned long error;
 } kee_aslr_set;
-#pragma pack()
+//#pragma pack()
 
 #pragma pack(1)
 typedef struct
